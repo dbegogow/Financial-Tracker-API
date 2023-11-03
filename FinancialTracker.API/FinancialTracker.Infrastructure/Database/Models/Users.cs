9a3e6d717792ce1bc7eed192c9725d6d6cebde7a
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-11-03 23:45:53 +0200

    Create User db entity

---
+++ FinancialTracker.API/FinancialTracker.Infrastructure/Database/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+using static FinancialTracker.Infrastructure.Database.ValidationConstants.User;
+
+namespace FinancialTracker.Infrastructure.Database.Models;
+
+public class Users : IdentityUser
+{
+    [Required]
+    [StringLength(NameMaxLength, MinimumLength = NameMinLength)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(NameMaxLength, MinimumLength = NameMinLength)]
+    public string LastName { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+}
---
+++ FinancialTracker.API/FinancialTracker.Infrastructure/Database/ValidationConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace FinancialTracker.Infrastructure.Database;
+
+public class ValidationConstants
+{
+    public class User
+    {
+        public const int NameMinLength = 3;
+        public const int NameMaxLength = 50;
+    }
+}
