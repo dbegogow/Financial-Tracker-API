4ca8cf2d83ea80863874bce664311e150162bbad
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-11-04 10:46:08 +0200

    Extend Result model

--- FinancialTracker.API/FinancialTracker.Data/Models/Result.cs
+++ FinancialTracker.API/FinancialTracker.Data/Models/Result.cs
@@ -1,10 +1,25 @@
 ﻿namespace FinancialTracker.Data.Models;
 
+using System.Net;
+
 public class Result<T>
 {
     private readonly List<string> errors;
 
+    public Result(T data)
+        : this(data, HttpStatusCode.OK)
+    {
+    }
+
+    public Result(T data, HttpStatusCode httpStatusCode)
+    {
+        this.Data = data;
+        this.StatusCode = httpStatusCode;
+    }
+
     public T Data { get; set; }
+
+    public HttpStatusCode StatusCode { get; set; }
 
     public IReadOnlyCollection<string> Errors
         => this.errors;
