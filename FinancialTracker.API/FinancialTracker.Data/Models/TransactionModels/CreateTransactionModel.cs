3f650d56391ee674427d64875b66f4fcc9072c7a
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-11-05 11:00:03 +0200

    Create model for creating transaction

---
+++ FinancialTracker.API/FinancialTracker.Data/Models/TransactionModels/CreateTransactionModel.cs
@@ -0,0 +1,18 @@
+﻿namespace FinancialTracker.Data.Models.TransactionModels;
+
+public class CreateTransactionModel
+{
+    public string UserId { get; init; }
+
+    public decimal Amount { get; init; }
+
+    public string Description { get; init; }
+
+    public DateTime Date { get; init; }
+
+    public Guid CategoryIdentifier { get; init; }
+
+    public Guid TransactionTypeIdentifier { get; init; }
+
+    public DateTime CreatedAt { get; init; }
+}
