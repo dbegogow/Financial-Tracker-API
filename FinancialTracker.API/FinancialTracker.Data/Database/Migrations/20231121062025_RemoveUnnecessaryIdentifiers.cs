e8aecd33ef829ae279a1b8fbc979317616ff8cb9
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-11-21 08:21:51 +0200

    Add new migration for removing unnecessary identifiers

---
+++ FinancialTracker.API/FinancialTracker.Data/Database/Migrations/20231121062025_RemoveUnnecessaryIdentifiers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinancialTracker.Data.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUnnecessaryIdentifiers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "Identifier",
+                table: "Categories");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Identifier",
+                table: "Categories",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
